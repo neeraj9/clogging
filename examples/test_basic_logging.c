c26f0102bb7e2b095ea855f3007b28d69bf9f704
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2015-05-29 16:19:15 +0530

    fix things so that we use the test-driver provied by gnu and run a simple test.

--- examples/test_basic_logging.c
+++ examples/test_basic_logging.c
@@ -1,6 +1,10 @@
+#include "../src/logging.h"
+
 #include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+	init_logging(argv[0], "", LOG_LEVEL_DEBUG);
+	LOG_DEBUG("A basic debug log looks like this");
 	return 0;
 }
