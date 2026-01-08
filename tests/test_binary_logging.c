d58f2ce382d364090bf8b5269afffc2840ecd268
Neeraj Sharma <neerajsharma.9@outlook.com>
2026-01-08 10:31:25 +0530

    update license

--- tests/test_binary_logging.c
+++ tests/test_binary_logging.c
@@ -1,20 +1,9 @@
 /*
- * Copyright (c) 2015 Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
+ * Copyright (c) 2026 Neeraj Sharma <neerajsharma.9@outlook.com>
  *
  *  This file is part of clogging.
  *
- *  clogging is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 2.1 of the License, or
- *  (at your option) any later version.
- *
- *  clogging is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with clogging.  If not, see <http://www.gnu.org/licenses/>.
+ *  See LICENSE file for licensing information.
  */
 
 #include "../src/binary_logging.h"
